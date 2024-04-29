ee9aebb162671547b34a382456b770ac9635f5b7
Damistar5 <117092162+Damistar5@users.noreply.github.com>
2024-04-29 15:06:19 +0100

    Delete data.py

--- data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_happiness_data():
-    happiness_data = pd.read_csv('world-happiness-report-2021.csv')
-    happiness_data.rename(columns={'Country': 'Country', 'Ladder Score': 'Healthy_life_expectancy'}, inplace=True)
-    return happiness_data
